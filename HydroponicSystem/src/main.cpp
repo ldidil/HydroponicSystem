94fb7a2df2711c2f027b5d019befbafc2c277771
ldidil <dominila.l@gmail.com>
2022-10-24 19:59:32 +0200

    Create Wifi connection

--- HydroponicSystem/src/main.cpp
+++ HydroponicSystem/src/main.cpp
@@ -1,15 +1,27 @@
 #include <Arduino.h>
+#include <WiFiMulti.h>
+
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+WiFiMulti wiFiMulti;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(921600);
   Serial.println("Hello from the setup");
+
+  wiFiMulti.addAP(WIFI_SSID,WIFI_PASS);
+  while (wiFiMulti.run() != WL_CONNECTED){
+    delay(500);
+  }
+
+  Serial.println("Conected");
 }
 
 void loop() {
   delay(1000);
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("Hello from the loop");
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
 }
