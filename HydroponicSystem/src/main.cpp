91d4d0e116f7917c7a7f2a0c1230fef85e1c3ee1
ldidil <dominila.l@gmail.com>
2022-10-25 18:00:46 +0200

    Add WebSocketsClient

--- HydroponicSystem/src/main.cpp
+++ HydroponicSystem/src/main.cpp
@@ -1,10 +1,45 @@
 #include <Arduino.h>
 #include <WiFiMulti.h>
+#include <WebSocketsClient.h>
 
 #define WIFI_SSID ""
 #define WIFI_PASS ""
 
 WiFiMulti wiFiMulti;
+WebSocketsClient wsClient;
+
+#define WS_HOST ""
+#define WS_PORT 443
+#define  WS_URL ""
+
+void diodBlink(){
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void handleMessage(uint8_t * payload){
+
+}
+void onWSEvent(WStype_t type, uint8_t * payload, size_t length){
+  switch (type){
+  case WStype_CONNECTED:
+    Serial.println("WS conntected");
+    break;
+    case WStype_DISCONNECTED:
+   Serial.println("WS disconntected");
+    break;
+    case WStype_TEXT: //client give us msg from server
+    Serial.printf("WS msg: %s\n",payload);
+    handleMessage(payload);
+
+
+    break;
+  default:
+    break;
+  }
+}
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -15,13 +50,13 @@ void setup() {
   while (wiFiMulti.run() != WL_CONNECTED){
     delay(500);
   }
-
   Serial.println("Conected");
+  wsClient.beginSSL(WS_HOST, WS_PORT, WS_URL, "", "wss");
+  wsClient.onEvent(onWSEvent);
 }
 
 void loop() {
-  delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  diodBlink();
+  wsClient.loop(); //keep the connection alive
+
 }
